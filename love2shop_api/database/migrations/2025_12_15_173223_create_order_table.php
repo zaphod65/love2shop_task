e8ff8b7405d12b959093d38a67cb58bf9412dcc7
Douglas Graham <douglas.r.graham.6@gmail.com>
2025-12-15 17:41:01 +0000

    BE: migrations.

---
+++ love2shop_api/database/migrations/2025_12_15_173223_create_order_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    private const TABLE = 'orders';
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(self::TABLE, function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE);
+    }
+};
---
+++ love2shop_api/database/migrations/2025_12_15_173348_create_order_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    private const TABLE = 'order_items';
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Table intended as a join table between orders and a hypothetical items table that
+        // is not implemented here
+        Schema::create(self::TABLE, function (Blueprint $table) {
+            $table->id();
+            $table->integer('order_id')->unsigned();
+            $table->integer('item_id');
+            $table->timestamps();
+
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE);
+    }
+};
